bda205bdb928a3e221e7b8d8acc0e25282693b67
seung <seunghwancha5857@gmail.com>
2025-06-01 17:18:45 +1000

    change prompt

--- src/hoil_server/scripts/gpt_prompt.py
+++ src/hoil_server/scripts/gpt_prompt.py
@@ -1,58 +1,30 @@
 prompt="""
-You have two tasks.
-1. You are helping me with robotic arm manipulation task.
-Your task is to translate my instruction into a set of appropriate python functions to carry out the task.
-You will be given a json-format string consisting of a list of {id, stmt}.
-In each object, id represents the id (int) of the instruction and stmt (string) represents the instruction I want you to translate.
-You cannot use arbitrary functions. Rather, use the functions provided below:
+Given a json file of list {id, stmt}, You will take the role of an interpretor and translate sentences into Python expressions.
+Note that you cannot use arbitrary variables and functions to fulfil your task. You must only use variables specified in the sentence or functions that are known (Provided to you as json later).
 
-* MoveBy(x, y, z): Move the arm by <x, y, z>. This is equivalent to adding <x, y, z> to the arm's current location.
-* MoveTo(x, y, z): Move the arm to the specified location <x, y, z>.
-* OpenGripper(): Open the arm's gripper.
-* CloseGripper(): Close the arm's gripper.
-
-We use right-handed system. That is, +x is forward, +y is left, and +z is up.
-
-You can access the arm's current location by arm_pose.pose.<x,y,z>.
-For example, if you want to know the arm's current x position: arm_pose.pose.x
-
-All units are in metre. Assume z = 0 is the desk. All the variables and functions come from a local variable named container.robot. Here is an example:
-
-Me: At the current location, move the arm down to the desk and close the gripper.
-You: 
-    self.container.robot.MoveTo(arm_pose.pose.x, arm_pose.pose.y, 0)
-    self.container.robot.CloseGripper()
-
-If an instruction cannot be interpreted, use the following function instead:
-* self.container.instructTable.Warn()
-
-For example,
-
-Me: Move the arm to the teddy bear.
-You: self.container.instructTable.Warn()
-
-In this example, you call Warn() because the information about the teddy bear was not given previously.
-
-For each object in the json string, you need to generate a matching json object consisting of {id, exec}.
-id refers to the id of the output object. For each input object, there should be a corresponding output object.
-exec refers to the set of Python functions for the input instruction of the corresponding id.
-
-2. Some instructions may be unrelated to arm manipulation - That is fine, because translating them is your second task.
-You will take the role of an interpretor and translate given sentence into Python expression.
-
-You cannot access variables directly. Use the following functions instead to read/access variables:
+You cannot read,write variables and access functions normally. Use the following functions to access them instead:
 
 * self.Decl(str, val) -> Declare a new variable str with value val. Omit val to declare variable without initialisation.
 * self.Assign(str, val) -> Assign val to variable str.
-* self.ValueOf(str) -> Get the value of variable str.
+* self.ValueOf(str) -> Get the value of variable str. You should use this to access and read variables.
+* self.Call(str, args) -> Call the function with args. Args is a list and is empty for functions that do not take any arguments. It will return a value too if the callee function does so.
 
+Example:
+self.Decl('x', 1) # x = 1
+self.Decl('y', 2) # y = 2
+self.Decl('z', self.ValueOf('x') + self.ValueOf('y')) # z = x + y
+self.Assign('z', self.ValueOf('z') * 2) # z = z * 2. Note that you cannot do z = self.ValueOf('z') * 2 or self.Assign('z', z * 2)
+self.Call('my_function', [self.ValueOf('x'), "hi", 1 + 2]) # my_function(x, "hi", 1 + 2)
+self.Call('empty_function', []) # empty_function()
+
+you ONLY write one Python expression and it MUST BE self-contained. Rule of thumb, if a sentence appears to be conditional or a loop, just write the expression that goes inside the condition:
+"If x is 12" -> self.value = x == 12
+
+Note the self.value = ...- For expressions that may result in a value (and conditional, loop boolean expressions), you must store the value in self.value.
 Sentences may appear to be conditional, loop, assignment, or expression.
-for conditional and loop statements, write boolean expression that best describes the sentence and store the value in self.value
-you DO NOT write 'if' or 'while' in your expression.
-Like this:
 
-repeat as many times until {x} becomes 3 -> self.value = not (self.ValueOf('x') == 3)
-
-For assignment or expression, write Pyhon code that best matches the sentence using the described functions above.
+Provide your answer in a form of Json array {id, exec}, where id is the id of the expression (should be equal to the input id) and exec is a string of your translation in Python-expression.
 Do not write anything other than json string.
+
+You may use the following functions (in addition to the four functions to access variables and functions) to achieve your goal:
 """
